bc1e57ffbe006f2ddc2f4318ae8e994a54b41a2d
Matt Moriarity <matt@mattmoriarity.com>
2019-09-04 19:31:41 -0600

    Show threads in the shopping list

--- Threads/ShoppingListViewController.swift
+++ Threads/ShoppingListViewController.swift
@@ -10,13 +10,48 @@ import UIKit
 import CoreData
 
 class ShoppingListViewController: UITableViewController {
+    enum Section: CaseIterable {
+        case threads
+    }
     
     private var managedObjectContext: NSManagedObjectContext {
         return (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
     }
+    
+    private var fetchedResultsController: NSFetchedResultsController<Thread>!
+    private var dataSource: UITableViewDiffableDataSource<Section, Thread>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchedResultsController =
+            NSFetchedResultsController(fetchRequest: Thread.inShoppingListFetchRequest(),
+                                       managedObjectContext: managedObjectContext,
+                                       sectionNameKeyPath: nil,
+                                       cacheName: nil)
+        fetchedResultsController.delegate = self
+        
+        tableView.register(ThreadTableViewCell.nib, forCellReuseIdentifier: "Thread")
+        dataSource = TableViewDiffableDataSource(tableView: tableView) { tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Thread", for: indexPath) as! ThreadTableViewCell
+            cell.populate(item)
+            return cell
+        }
+        
+        do {
+            try fetchedResultsController.performFetch()
+            updateSnapshot()
+        } catch {
+            NSLog("Error fetching objects: \(error)")
+        }
+    }
+    
+    func updateSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Thread>()
+        snapshot.appendSections(Section.allCases)
+        let objects = fetchedResultsController.fetchedObjects ?? []
+        snapshot.appendItems(objects, toSection: .threads)
+        dataSource.apply(snapshot)
     }
 
     @IBAction func unwindCancelAdd(segue: UIStoryboardSegue) {
@@ -48,4 +83,22 @@ class ShoppingListViewController: UITableViewController {
         }
     }
 
+}
+
+extension ShoppingListViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        updateSnapshot()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .update:
+            let thread = anObject as! Thread
+            if let cell = tableView.cellForRow(at: indexPath!) as? ThreadTableViewCell {
+                cell.populate(thread)
+            }
+        default:
+            break
+        }
+    }
 }
