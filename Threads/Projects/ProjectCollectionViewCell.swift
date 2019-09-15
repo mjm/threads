81213090425f4a6bda1bba7e4cad06b2589e5c9e
Matt Moriarity <matt@mattmoriarity.com>
2019-09-15 13:06:52 -0600

    Show primary image on projects list screen.
    Also improved appearance of projects that have no images

--- Threads/Projects/ProjectCollectionViewCell.swift
+++ Threads/Projects/ProjectCollectionViewCell.swift
@@ -8,19 +8,34 @@
 
 import UIKit
 
+@IBDesignable
 class ProjectCollectionViewCell: UICollectionViewCell {
-    // TODO: make this a more custom view
-    @IBOutlet var colorView: UIView!
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        colorView.layer.cornerRadius = 10
+        imageView.layer.cornerRadius = 10
+        imageView.layer.cornerCurve = .continuous
+
+        imageView.tintColor = UIColor { traitCollection in
+            UIColor.systemBackground
+                .withAlphaComponent(traitCollection.userInterfaceStyle == .dark ? 0.4 : 0.6)
+        }
     }
     
     func populate(_ project: Project) {
         nameLabel.text = project.name
-        colorView.backgroundColor = .systemOrange
+
+        if let image = project.primaryImage?.image {
+            imageView.backgroundColor = nil
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = image
+        } else {
+            imageView.backgroundColor = UIColor.systemIndigo.withAlphaComponent(0.5)
+            imageView.contentMode = .center
+            imageView.image = UIImage(systemName: "photo")
+        }
     }
 }
