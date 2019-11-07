22476f66daaf0c6b3b82fe9829869e1190f5ff77
Matt Moriarity <matt@mattmoriarity.com>
2019-11-06 20:22:52 -0700

    More documentation

--- Threads/Common/Actions/BoundUserAction.swift
+++ Threads/Common/Actions/BoundUserAction.swift
@@ -19,6 +19,15 @@ struct BoundUserActionOptions: OptionSet {
     static let destructive = BoundUserActionOptions(rawValue: 1 << 0)
 }
 
+/// A wrapper for a type-erased user action that has been bound to a particular action runner.
+///
+/// This provides a way for view models, which own the action runner, to provide actions to the view controller that
+/// can be performed on their own, without needing access to the action runner. Because they are type-erased, it's
+/// possible to create a heterogenous collection of bound user actions (for showing a menu, for instance) or have a
+/// view model property expose an action whose underlying type is different depending on the state of the app.
+///
+/// Bound user actions can also be easily converted into various UIKit action types as necessary for different types
+/// of UI, like context menus and action sheets.
 struct BoundUserAction<ResultType> {
     typealias CanPerformHandler = () -> Bool
     typealias WillPerformHandler = () -> Void
@@ -34,6 +43,22 @@ struct BoundUserAction<ResultType> {
     fileprivate var willPerformBlock: WillPerformHandler = {}
     fileprivate var performBlock: PerformHandler
 
+    /// Create a bound action for an arbitrary code block.
+    ///
+    /// Only use this for really specific cases where you want a view model to expose an action that really only affects
+    /// local state, and therefore doesn't make sense to be a UserAction. A good example is toggling the editing state
+    /// of the view.
+    ///
+    /// - Parameters:
+    ///    - title: A localized string that will be displayed in UI elements generated from the action.
+    ///    - shortTitle: A localized string that will be used instead of `title` for space-constrained UI
+    ///      elements like table row swipe actions. Optional, `title` will be used if not provided.
+    ///    - options: Options to affect how UI elements are generated for the action.
+    ///    - canPerform: A block that reports whether the action is currently able to be performed. Affects
+    ///      whether UI elements for the action are enabled. If not provided, the action is always able to be
+    ///      performed.
+    ///    - perform: A block that performs the action.
+    ///
     init(
         title: String,
         shortTitle: String? = nil,
@@ -48,6 +73,19 @@ struct BoundUserAction<ResultType> {
         self.performBlock = perform
     }
 
+    /// Bind a `UserAction` to a `UserActionRunner`.
+    ///
+    /// You don't need to call this initializer directly. The `UserAction.bind(to:title:options:)` convenience
+    /// function is preferred in general.
+    ///
+    /// - Parameters:
+    ///    - action: The `UserAction` that is being bound.
+    ///    - runner: The `UserActionRunner` that will run the action when it is performed.
+    ///    - title: A localized string that will be displayed in UI elements generated from the action.
+    ///    - shortTitle: A localized string that will be used instead of `title` for space-constrained UI
+    ///      elements like table row swipe actions. Optional, `title` will be used if not provided.
+    ///    - options: Options to affect how UI elements are generated for the action.
+    ///
     init<Action: UserAction>(
         _ action: Action,
         runner: UserActionRunner,
@@ -74,6 +112,12 @@ struct BoundUserAction<ResultType> {
         )
     }
 
+    /// Whether the action should be marked as destructive in UI.
+    ///
+    /// In context menus and action sheets, this causes the text for the action to be shown in red.
+    ///
+    /// This can be set as creation time with the `.destructive` option.
+    ///
     var isDestructive: Bool {
         get {
             options.contains(.destructive)
@@ -87,8 +131,17 @@ struct BoundUserAction<ResultType> {
         }
     }
 
+    /// Whether the action can currently be performed.
+    ///
     var canPerform: Bool { canPerformBlock() }
 
+    /// Perform the action immediately.
+    ///
+    /// - Parameters:
+    ///    - source: A UI which view controllers that this action presents will use as their source.
+    ///    - willPerform: An optional block that will run before the action's work is performed.
+    /// - Returns: A publisher that will send a single result value when the action completes.
+    ///
     @discardableResult
     func perform(source: UserActionSource? = nil, willPerform: @escaping () -> Void = {})
         -> AnyPublisher<ResultType, Error>
@@ -102,6 +155,14 @@ struct BoundUserAction<ResultType> {
             })
     }
 
+    /// Attach additional work to happen before the action is performed.
+    ///
+    /// The view model can use this to schedule work to occur if the user chooses to perform the action.
+    ///
+    /// - Parameters:
+    ///    - block: The work to perform before the action.
+    /// - Returns: A copy of the action with the additional work attached.
+    ///
     func onWillPerform(_ block: @escaping () -> Void) -> Self {
         var newAction = self
 
--- Threads/Common/Combine/Operators.swift
+++ Threads/Common/Combine/Operators.swift
@@ -9,14 +9,28 @@
 import Combine
 
 extension Publisher {
+    /// Swallow errors, completing the stream immediately.
     func ignoreError() -> Publishers.Catch<Self, Empty<Output, Never>> {
         self.catch { _ in Empty(completeImmediately: true) }
     }
 
+    /// Transform the stream into one that produces optional values of the same time.
+    ///
+    /// This allows you to use a stream that produces non-optional values with a subscriber that is expecting optionals.
+    ///
     func optionally() -> AnyPublisher<Self.Output?, Self.Failure> {
         map { o -> Output? in o }.eraseToAnyPublisher()
     }
 
+    /// A variant of sink that keeps the subscription alive until it completes.
+    ///
+    /// Use this for publishers that are known to complete at some point in the near future when there is no appropriate
+    /// place to store the subscription. Using this with a publisher that doesn't complete will cause a memory leak.
+    ///
+    /// - Parameters:
+    ///    - receiveCompletion: The closure to execute on completion.
+    ///    - receiveValue: The closure to execute on receipt of a value.
+    ///
     func handle(
         receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void,
         receiveValue: @escaping (Output) -> Void
@@ -30,6 +44,24 @@ extension Publisher {
                 }, receiveValue: receiveValue)
     }
 
+    /// Modifies the collections in the output by applying lists of changes from another publisher.
+    ///
+    /// This is most useful when the publisher feeds this result back into itself, so that the next element in the stream
+    /// is the result of applying the last collection of changes.
+    ///
+    /// Use the `transform` closure to produce a collection of elements parallel to the elements in the collection
+    /// producing the changes. The benefit of doing this instead of just mapping over the collection is that you only
+    /// create new elements when they were inserted in the original list: otherwise, the value remains the same.
+    ///
+    /// - Parameters:
+    ///    - changes: A publisher that emits collections of changes that should be applied to the collections
+    ///      that this publisher produces. The type of element for the changes can be different than the elements
+    ///      in those collections.
+    ///    - transform: A closure that is called on each inserted element found in `changes` that transforms
+    ///      it into the element that will inserted into the collections emitted by the returned publisher.
+    ///
+    /// - Returns: A new publisher that emits the collections from this publisher with the changes applied.
+    ///
     func applyingChanges<Changes: Publisher, ChangeItem>(
         _ changes: Changes,
         _ transform: @escaping (ChangeItem) -> Output.Element
@@ -56,16 +88,34 @@ extension Publisher {
 }
 
 extension Publisher where Output == Bool {
+    /// Inverts boolean values in the stream.
     func invert() -> AnyPublisher<Self.Output, Self.Failure> {
         map { !$0 }.eraseToAnyPublisher()
     }
 }
 
 extension Publisher where Failure == Never {
+    /// A variant of sink that keeps the subscription alive until it completes.
+    ///
+    /// Use this for publishers that are known to complete at some point in the near future when there is no appropriate
+    /// place to store the subscription. Using this with a publisher that doesn't complete will cause a memory leak.
+    ///
+    /// - Parameters:
+    ///    - receiveValue: The closure to execute on receipt of a value.
+    ///
     func handle(receiveValue: @escaping (Output) -> Void) {
         handle(receiveCompletion: { _ in }, receiveValue: receiveValue)
     }
 
+    /// Assign the output to a property of an object.
+    ///
+    /// - Parameters:
+    ///    - keyPath: A key path indicating the property to assign.
+    ///    - object: The object that contains the property.
+    ///    - weak: Whether to hold `object` weakly or not. Use this to avoid cycles.
+    ///
+    /// - Returns:A subscriber that assigns the value to the property.
+    ///
     func assign<Root: AnyObject>(
         to keyPath: ReferenceWritableKeyPath<Root, Output>,
         on object: Root,
